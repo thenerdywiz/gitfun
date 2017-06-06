6bc121de6355ce6b6f668020537a8f02e9e2b8e5
Cute Dog <thenerdywiz@gmail.com>
2017-06-05 23:03:21 -0400

    region zoom
    zooming into region

--- Pokemon2/Pokemon2/ViewController.swift
+++ Pokemon2/Pokemon2/ViewController.swift
@@ -7,19 +7,59 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var MapView: MKMapView!
+    
+    var updateCount = 0
+    
+    var manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        manager.delegate = self as CLLocationManagerDelegate
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse
+        {
+         print("Ready to Go")
+            
+            MapView.showsUserLocation = true
+            manager.startUpdatingLocation()
+            
+        }else{
+        
+        manager.requestWhenInUseAuthorization()
+        
+        
+        }
+        
+            }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        
+        if updateCount < 1 {
+        
+        let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 2000,2000)
+        
+        MapView .setRegion(region, animated: false)
+        
+        updateCount += 1
+        
+        
+        print("We made it")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+
+
+    }
 
 }
 
